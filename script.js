355b8f7f14cbecf093b7998c8474c0cedc8975fa
marco987 <pacemarco87@gmail.com>
2019-07-09 18:20:53 +0200

    Push delle 18:20

--- script.js
+++ script.js
@@ -9,23 +9,45 @@
 // il software deve comunicare il punteggio, cioè
 // il numero di volte che l’utente ha inserito un numero consentito.
 
-var contenitore_random_16 = [], num_utente;
+var contenitore_random16 = [], num_utente;
 
+// Funzione che genera i numeri random
 function random16(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var numeri = contenitore_random_16;
+// Ciclo che genera 16 numeri differenti
 var i = 0;
 while ( i < 16 ) {
   var n = random16(1, 101);
-  var r = numeri.indexOf(n);
+  var r = contenitore_random16.indexOf(n);
   if (r < 0) {
-  numeri.push(n);
+  contenitore_random16.push(n);
   i++;
   }
 }
 
-console.log(contenitore_random_16);
+// Elenco 16 numeri estratti
+console.log("Numeri estratti: " + contenitore_random16);
+
+var haiPerso;
+
+for (var j = 0; j < 84; j++) {
+  numeroScelto = parseInt(prompt("Inserisci un numero da 1 a 100"));
+  if (numeroScelto === contenitore_random16[j]) {
+    haiPerso = "Hai perso!";
+  }
+}
+
+//     if (numeroScelto === contenitore_random16[j]) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+// }
+
+// richiestaNum(num_utente);
+// console.log(richiestaNum(num_utente));
